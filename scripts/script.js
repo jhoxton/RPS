5f6044e53768b6b3385450eccb12583527b2f332
jhoxton <jhsn@protonmail.com>
2022-06-13 21:12:42 +1000

    Started new game logic

--- scripts/script.js
+++ scripts/script.js
@@ -1,109 +1,43 @@
-
-
-const choice = ['Rock', 'Paper', 'Scissors'];
-let gameCount = 1;
-
+//Game variables
+const choice = ['rock', 'paper', 'scissors'];
 let playerWins = 0;
 let computerWins = 0;
+let gameCount = 0;
 
-document.getElementById('round_number').innerHTML = gameCount;
-
-document.getElementById('player_score').innerHTML = playerWins;
-document.getElementById('comp_score').innerHTML = computerWins;
-
-
-let compSelWrap = document.querySelector("#comp_choice")
-compSelWrap.textContent = computerPlay();
+let playerSelection;
+let compSelection;
 
 
-// Rock Paper Scissors 
+//DOM elements
+document.getElementById("rock").addEventListener("click", () => updatePlayerChoice('Rock'));
+document.getElementById("paper").addEventListener("click", () => updatePlayerChoice('Paper'));
+document.getElementById("scissors").addEventListener("click", () => updatePlayerChoice('Scissors'));
 
-
-playerSelection= 0;
-// let gameCount = 1;
-
-
-function game(){
-  for(i=0;i<5;i++){
-    let playerSelection = checkInput();
-    let computerSelection = computerPlay();
-    playRound(playerSelection, computerSelection);
-    console.log("Round: ", gameCount++);
-  }
-  checkScore();
+//Sets player choice, then runs the game
+function updatePlayerChoice(input){
+    if(input === "Rock"){
+        playerSelection = "Rock";
+    } else if (input ==="Paper"){
+        playerSelection = "Paper";
+    } else if (input ==="Scissors"){
+        playerSelection = "Scissors";
+    } else{
+        
+    }
+    compSelection = updateCompSelection(); //Random comp choice
+    playRound(playerSelection, compSelection); //Runs the game
 }
 
-//Checking player input
-function checkInput(playerSelection) {
-  // playerSelection = prompt("Choose Rock, Paper, or Scissors");
-  playerSelection = playerSelection.toLowerCase();
-  return playerSelection;
-}
-  
-// Getting computer input
-function computerPlay(){
-  let compResult = choice[Math.floor(Math.random() * choice.length)];
-  return compResult;
-}
-  
+function playRound(playerSelection, compSelection) {
+    gameCount ++;
+    console.log("Game: ", gameCount);
+    console.log("Player: ",playerSelection);
+    console.log("Comp: ",compSelection);
 
-function checkScore() {
-  if(playerWins < computerWins) {
-    console.log("You won");
-  } else if(computerWins < playerWins) {
-    console.log("You lost");
-  } else if (playerWins === computerWins){
-    console.log("It was a draw");
-  }
- }
-
-
-
-function playRound(playerSelection, computerSelection) {
-  
-  if(playerSelection == "Paper" && computerSelection == 'Rock'){
-    console.log("You win, Paper beats Rock");
-    playerWins ++;
-  } 
-  else if (playerSelection == "Scissors" && computerSelection == 'Paper'){
-    console.log("You win, Scissors beats Rock ");
-    playerWins ++;
-
-  }
-  else if (playerSelection == "Rock" && computerSelection == 'Scissors'){
-    console.log("You win, Rock beats Scissors ");
-    playerWins ++;
-
-  }
-
-  else if(playerSelection == "Rock" && computerSelection =='Paper'){
-    console.log("You lose, Paper beats Rock");
-    computerWins ++;
-  } 
-  else if (playerSelection == "Paper" && computerSelection == 'Scissors'){
-    console.log("You lose, Scissors beats Paper ");
-    computerWins ++;
-
-  }
-  else if (playerSelection == "Scissors" && computerSelection == 'Rock'){
-    console.log("You lose, Rock beats Scissors ");
-    computerWins ++;
-  }
-
-  else if(playerSelection == "Scissors" && computerSelection == 'Scissors'){
-    console.log("Draw: Scissors v Scissors");
-  } 
-  else if (playerSelection == "Rock" && computerSelection == 'Rock'){
-    console.log("Draw: Rock v Rock")
-  }
-  else if (playerSelection == "Paper" && computerSelection == 'Paper'){
-    console.log("Draw: Paper v Paper")
-    
-  }
-  playerSelection = 0;
-  computerSelection = 0;
 }
 
-
-
-// game();
+//Random comp pick
+function updateCompSelection(compSelection){ 
+    let compResult = choice[Math.floor(Math.random() * choice.length)];
+    return compResult;
+  }
